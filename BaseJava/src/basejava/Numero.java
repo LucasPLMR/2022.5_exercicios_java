97109da127fb4b86a9ed3fc21d454a857e109b8c
Técnico em Informática - 2022.5 <169127512022.5@senacrjedu.local>
2023-03-13 19:37:20 -0300

    inicio

---
+++ BaseJava/src/basejava/Numero.java
@@ -0,0 +1,14 @@
+
+package basejava;
+
+
+public class Numero {
+ int valor;
+ public String parImpar (){
+     if(this.valor % 2 ==0){
+         return "PAR";
+     }else{
+         return "IMPAR";
+     }
+ }
+}
---
+++ BaseJava/src/basejava/Principal.java
@@ -0,0 +1,21 @@
+
+package basejava;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+          //  int valor = 3;
+          //  if (valor % 2 == 0) {
+          //      System.out.print("PAR");
+         //   }else{
+         //       System.out.print("IMPAR");  
+         //   };
+            Numero numero = new Numero();
+            numero.valor =4;
+            System.out.print (numero.parImpar());
+                    
+        
+    }
+    
+}
